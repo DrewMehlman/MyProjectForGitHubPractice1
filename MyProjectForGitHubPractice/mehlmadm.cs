1f50f5c30fead719af454b2d6a0cd782e54f73d5
Andrew Mehlman <mehlmadm@mail.uc.edu>
2024-04-02 12:02:20 -0400

    Add a write line saying hello

---
+++ MyProjectForGitHubPractice/mehlmadm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProjectForGitHubPractice
+{
+    internal class mehlmadm
+    {
+        public mehlmadm() {
+            Console.WriteLine("Hello");
+        }
+    }
+}
